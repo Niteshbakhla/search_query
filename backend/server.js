feef0316a12126419da34dbd314142d67dc19478
enter <niteshbakhla007@gmail.com>
2025-04-09 14:50:46 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,15 @@ const searchRouter = require("./src/routes/route");
 const cors = require("cors")
 connectDB()
 
+const PORT = process.env.PORT || 5000
 
 app.use(cors({
             origin: process.env.FRONTEND_URL,
 }));
 app.use("/api", searchRouter)
 
-app.listen(3000, () => {
-            console.log("Server is running at port 3000")
+app.listen(PORT, () => {
+            console.log(`Server is running at port ${PORT}`)
 })
 
 
